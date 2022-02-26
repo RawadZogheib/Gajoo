b28634b05fbd7c258d7dfca21565452e2fc47ed3
zogheibRawad <rawadzogheib@gmail.com>
2022-02-26 22:11:37 +0200

    .

---
+++ lib/page/HomePage.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:gajoo/globals/globals.dart' as globals;
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
