8a7c6c83e815838acf9df94a97eb8af3b9c4275e
zogheibRawad <rawadzogheib@gmail.com>
2022-02-26 22:17:21 +0200

    Update globals.dart

--- lib/globals/globals.dart
+++ lib/globals/globals.dart
@@ -62,35 +62,6 @@ const String errorException =
 const String errorVersion = "New version available.";
 const String rememberMe =
     "Stay signed in?\n Do this to reduce the number of times you are asked to sign in.";
-// //Errors
-// const String error1 = "No Spaces Allowed.";
-// const String error2_1 = "Your username must contain at least 8 characters.";
-// const String error2_2 = "Your username can only contain lowercase and uppercase characters and special characters( _ .).";
-// const String error2_3 = "Your password must contain at least 8 characters, 1 lowercase(a-z),1 uppercase(A-Z),1 numeric character(0-9) and 1 special character(* . ! @ # \$ % ^ & : , ? _ -).";
-// //const String error2_4 = "Your age must be greater than 17.";
-// const String error2_5 = "It's not an email format.";
-// //const String error2_6 = "It's not a university email.";
-// const String error2_7 = "phone Number already exists";
-// const String error3 = "Please make sure your passwords match.";
-// const String error4 = "Cannot connect to the dataBase.";    // error in insert or delete or update etc...
-// const String error5 = "UserName already exist.";
-// const String error6 = "Email already exist.";
-// const String error7 = "Field cannot be empty.";
-// const String error8 = "Incorrect email or password.";       // for login
-// const String error9 = "It's not a phoneNumber format, example: +961########";
-// const String error10 = "No data available";
-// const String error11 = "This email is not Registered";
-// const String error12 = "Incorrect Email sent";
-// const String error401 = "Client Wallet Error";
-// const String error402 = "Consultant wallet Error";
-// const String error403 = "You don't have enough bill";
-// const String errorToken = "Token Error.";
-// const String errorVersion = "New version available.";
-// const String errorElse = "Failed to connect... Connection Problem.";
-// const String errorException = "OOPs! Something went wrong. Try again in few seconds.";
-// const String codeFailed = "your code is incorrect";
-// const String codeException = "Oops Something went wrong! please try to login again!";
-
 
 //for 6 digit code
 String? sixCodeNb = null;
