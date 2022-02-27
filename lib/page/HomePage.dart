e07a411cb09e8d1b0c88b535a3e2c267aa690042
zogheibRawad <rawadzogheib@gmail.com>
2022-02-27 22:37:17 +0200

    Update HomePage.dart

--- lib/page/HomePage.dart
+++ lib/page/HomePage.dart
@@ -108,7 +108,7 @@ class _HomePageState extends State<HomePage> {
                       ],
                     ),
                     const SizedBox(
-                      height: 20,
+                      height: 200,
                     ),
                     Center(
                       child: AnimatedLogo(
