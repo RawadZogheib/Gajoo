726ec7dde619cf99a93d6e232bc3489886757fae
Michel115 <91220948+Michel115@users.noreply.github.com>
2022-03-02 17:51:15 +0200

    Create Teacher.dart

---
+++ lib/page/Teacher.dart
@@ -0,0 +1,22 @@
+
+
+
+import 'package:flutter/cupertino.dart';
+
+class Teacher extends StatefulWidget {
+  String? type;
+  String? languages;
+  String? level;
+
+  Teacher({this.type, this.languages, this.level});
+
+  @override
+  _TeacherState createState() => _TeacherState();
+}
+
+class _TeacherState extends State<Teacher> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
